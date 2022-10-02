034988b7e7894167b799c114bd0502364cda5134
Lucas Gabriel <llucas182011@hotmail.com>
2022-10-02 08:17:29 -0300

    settings

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import { GlobalStyle } from './components/Styles/global.js';
 import { ThemeProvider } from 'styled-components';
-import { purpleTheme } from './components/Styles/themes/themes';
+import * as theme from './components/Styles/themes/themes';
 
 import MainRoutes from './routes/routes.js';
 
 export default function App() {
 	return (
-		<ThemeProvider theme={purpleTheme}>
+		<ThemeProvider theme={theme.purpleTheme}>
 			<Router>
 				<MainRoutes/>
 			</Router>
